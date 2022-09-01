f592649d76934c148e8726681041ce4d7ffe3e7f
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 01:39:43 +0200

    javascript action

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-console.log("TEST")
+const core = require('@actions/core');
+const github = require('@actions/github');
+const { exec } = require("node:child_process");
 
-// NEW CHANGE
+try {
+    exec("ls ./", (err, output) => {
+        if (err) {
+            core.setFailed(err.message);
+            return
+        }
+        core.setOutput("ls", output);
+    })
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+//git diff --name-only --diff-filter=ACMRT ${{ github.event.before }} ${{ github.sha }} | xargs
