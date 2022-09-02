dd96c648fa4854334bd83c82cd1dd4fc228da79e
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 02:54:50 +0200

    .

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
             return
         }
         core.setOutput("all", output);
-        console.log(output)
+        core.debug(output)
     })
 } catch (error) {
     core.setFailed(error.message);
