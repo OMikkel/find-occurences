35827ebe3b0e8ba30f3dda7b0077da5bb75c8559
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 02:31:50 +0200

    .

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const { exec } = require("node:child_process");
 try {
     const before_sha = core.getInput("before-sha")
     const after_sha = core.getInput("after-sha")
-    // exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | xargs`, (err, output) => {
-    //     if (err) {
-    //         core.setFailed(err.message);
-    //         return
-    //     }
-    //     core.setOutput("all", output);
-    // })
-    exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | grep '\.tsx\|\.ts\|\.js\|\.jsx\?$' | xargs`, (err, output) => {
+    exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | grep .ts$|.js$|.tsx$|.jsx$ | xargs`, (err, output) => {
         if (err) {
             core.setFailed(err.message);
             return
