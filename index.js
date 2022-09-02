c387d76f65f346833fe217c4bb7da810a184aa3a
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 03:18:22 +0200

    .

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const { exec } = require("node:child_process");
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 try {
     const before_sha = core.getInput("before-sha")
     const after_sha = core.getInput("after-sha")
-    exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | grep -E '(.ts|.js|.tsx|.jsx)$' | xargs`, (err, output) => {
-        if (err) {
-            core.setFailed(err.message);
+    const { stdout, stderr } = await exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | grep -E '(.ts|.js|.tsx|.jsx)$' | xargs`);
+    if (stderr) {
+        core.setFailed(stderr);
+        return
+    }
+    core.setOutput("all", stdout);
+    const files = stdout.split(" ").map(file => file.trim())
+    core.debug(files)
+    files.map(file => {
+        const { stdout, stderr } = await exec("grep -E 'console.log|console.error' ./" + file);
+        if (stderr) {
+            core.setFailed(stderr);
             return
         }
-        core.setOutput("all", output);
-        const files = output.split(" ").map(file => file.trim())
-        core.debug(files)
+        core.debug("stdout: " + stdout)
     })
 } catch (error) {
     core.setFailed(error.message);
