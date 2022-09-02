c926c2f2e166e2086c8487681230cfbcf2db94e8
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 02:00:20 +0200

    .

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const github = require('@actions/github');
 const { exec } = require("node:child_process");
 
 try {
-    exec("ls ./", (err, output) => {
+    const before_sha = core.getInput("before-sha")
+    const after_sha = core.getInput("after-sha")
+    exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | xargs`, (err, output) => {
         if (err) {
             core.setFailed(err.message);
             return
         }
-        core.setOutput("ls", output);
+        core.setOutput("all", output);
+    })
+    exec(`git diff --name-only --diff-filter=ACMRT ${ before_sha } ${ after_sha } | grep .ts$ | xargs)`, (err, output) => {
+        if (err) {
+            core.setFailed(err.message);
+            return
+        }
+        core.setOutput("ts", output);
     })
 } catch (error) {
     core.setFailed(error.message);
 }
-
-//git diff --name-only --diff-filter=ACMRT ${{ github.event.before }} ${{ github.sha }} | xargs
