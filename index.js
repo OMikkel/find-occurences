c9651dec0fe8758606b0835c4bbd537e678a1dd0
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 15:47:47 +0200

    .

--- index.js
+++ index.js
@@ -10,42 +10,52 @@ const run = async () => {
         const search_term = core.getInput("search-term")
         const file_types = core.getInput("file-types")
         const should_fail = core.getInput("should-fail")
+        const check_all = core.getInput("check-all")
         var occurences = ""
         var occurences_count = 0
 
         try {
-            const { stdout, stderr } = await exec(`git diff --name-only --diff-filter=ACMRT ${ before }...${ after } | grep -E '${ file_types }' | xargs`);
-            if (stderr) {
-                core.setFailed(stderr);
-                return
-            }
-            const files = stdout.split(" ").map(file => file.trim())
-            core.debug(files)
-
-            const mapFiles = files.map(async file => {
-                try {
-                    const { stdout, stderr } = await exec(`grep --color=always -H -n -E '${ search_term }' ./${ file }`);
-                    if (stderr) {
-                        return
-                    }
-                    core.debug("stdout: " + stdout)
-                    occurences_count += stdout.split("\n").length - 1
-                    occurences += stdout + "\n"
-                } catch (error) {
+            if (check_all == "true") {
+                const { stdout, stderr } = await exec(`git ls-files"`);
+                if (stderr) {
+                    core.setFailed(stderr);
+                    return;
+                }
+                core.setOutput("all", stdout);
+            }else{
+                const { stdout, stderr } = await exec(`git diff --name-only --diff-filter=ACMRT ${ before }...${ after } | grep -E '${ file_types }' | xargs`);
+                if (stderr) {
+                    core.setFailed(stderr);
                     return
                 }
-            })
-
-            return Promise.all(mapFiles)
-            .then(() => {
-                if (occurences.length > 0) {
-                    if (should_fail == "true") {
-                        core.setFailed(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
-                    }else{
-                        core.warning(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
+                const files = stdout.split(" ").map(file => file.trim())
+                core.debug(files)
+    
+                const mapFiles = files.map(async file => {
+                    try {
+                        const { stdout, stderr } = await exec(`grep --color=always -H -n -E '${ search_term }' ./${ file }`);
+                        if (stderr) {
+                            return
+                        }
+                        core.debug("stdout: " + stdout)
+                        occurences_count += stdout.split("\n").length - 1
+                        occurences += stdout + "\n"
+                    } catch (error) {
+                        return
                     }
-                }
-            })
+                })
+    
+                return Promise.all(mapFiles)
+                .then(() => {
+                    if (occurences.length > 0) {
+                        if (should_fail == "true") {
+                            core.setFailed(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
+                        }else{
+                            core.warning(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
+                        }
+                    }
+                })
+            }
         } catch (error) {
             core.setFailed(error.message);
             return
