4c65e0e6fd5abd8288b60c26941e99e17ca15e1e
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 03:27:58 +0200

    .

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const util = require('node:util');
+const exec = util.promisify(require('node:child_process').exec);
 
 const run = async () => {
     try {
