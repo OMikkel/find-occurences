610ab36f9c26a9b9e9e2c8d440ba03d10c1ec769
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 16:05:32 +0200

    .

--- index.js
+++ index.js
@@ -16,12 +16,41 @@ const run = async () => {
 
         try {
             if (check_all == "true") {
-                const { stdout, stderr } = await exec(`git ls-files --exclude=./node_modules/*`);
+                const { stdout, stderr } = await exec(`git ls-files -- ':!:node_modules/*' | grep -E '${ file_types }' | xargs`);
                 if (stderr) {
                     core.setFailed(stderr);
                     return;
                 }
                 core.setOutput("all", stdout);
+
+                const files = stdout.split(" ").map(file => file.trim())
+                core.debug(files)
+    
+                const mapFiles = files.map(async file => {
+                    try {
+                        const { stdout, stderr } = await exec(`grep --color=always -H -n -E '${ search_term }' ./${ file }`);
+                        if (stderr) {
+                            return
+                        }
+                        core.debug("stdout: " + stdout)
+                        occurences_count += stdout.split("\n").length - 1
+                        occurences += stdout + "\n"
+                    } catch (error) {
+                        return
+                    }
+                })
+    
+                return Promise.all(mapFiles)
+                .then(() => {
+                    if (occurences.length > 0) {
+                        if (should_fail == "true") {
+                            core.setFailed(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
+                        }else{
+                            core.warning(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
+                        }
+                    }
+                })
+
             }else{
                 const { stdout, stderr } = await exec(`git diff --name-only --diff-filter=ACMRT ${ before }...${ after } | grep -E '${ file_types }' | xargs`);
                 if (stderr) {
