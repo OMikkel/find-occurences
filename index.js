79b982d47d3be94b08797aac0a3b262f86f35a63
OMikkel <Mikkelmaae@gmail.com>
2022-09-02 16:24:15 +0200

    Changes to code structure

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const util = require('node:util');
-const exec = util.promisify(require('node:child_process').exec);
+const { check_all_fnc } = require('./check_all');
+const { check_diff_fnc } = require('./check_diff');
 
 const run = async () => {
     try {
@@ -11,86 +10,18 @@ const run = async () => {
         const file_types = core.getInput("file-types")
         const should_fail = core.getInput("should-fail")
         const check_all = core.getInput("check-all")
-        var occurences = ""
-        var occurences_count = 0
+        const check_all_exclude = core.getInput("check-all-exclude")
 
         try {
             if (check_all == "true") {
-                const { stdout, stderr } = await exec(`git ls-files -- ':!:node_modules/*' | grep -E '${ file_types }' | xargs`);
-                if (stderr) {
-                    core.setFailed(stderr);
-                    return;
-                }
-                core.setOutput("all", stdout);
-
-                const files = stdout.split(" ").map(file => file.trim())
-                core.debug(files)
-    
-                const mapFiles = files.map(async file => {
-                    try {
-                        const { stdout, stderr } = await exec(`grep --color=always -H -n -E '${ search_term }' ./${ file }`);
-                        if (stderr) {
-                            return
-                        }
-                        core.debug("stdout: " + stdout)
-                        occurences_count += stdout.split("\n").length - 1
-                        occurences += stdout + "\n"
-                    } catch (error) {
-                        return
-                    }
-                })
-    
-                return Promise.all(mapFiles)
-                .then(() => {
-                    if (occurences.length > 0) {
-                        if (should_fail == "true") {
-                            core.setFailed(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
-                        }else{
-                            core.warning(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
-                        }
-                    }
-                })
-
+                await check_all_fnc(check_all_exclude, file_types, search_term, should_fail)
             }else{
-                const { stdout, stderr } = await exec(`git diff --name-only --diff-filter=ACMRT ${ before }...${ after } | grep -E '${ file_types }' | xargs`);
-                if (stderr) {
-                    core.setFailed(stderr);
-                    return
-                }
-                const files = stdout.split(" ").map(file => file.trim())
-                core.debug(files)
-    
-                const mapFiles = files.map(async file => {
-                    try {
-                        const { stdout, stderr } = await exec(`grep --color=always -H -n -E '${ search_term }' ./${ file }`);
-                        if (stderr) {
-                            return
-                        }
-                        core.debug("stdout: " + stdout)
-                        occurences_count += stdout.split("\n").length - 1
-                        occurences += stdout + "\n"
-                    } catch (error) {
-                        return
-                    }
-                })
-    
-                return Promise.all(mapFiles)
-                .then(() => {
-                    if (occurences.length > 0) {
-                        if (should_fail == "true") {
-                            core.setFailed(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
-                        }else{
-                            core.warning(`Found ${ occurences_count } occurences of ${ search_term }\n${ occurences }`);
-                        }
-                    }
-                })
+                await check_diff_fnc(before, after, file_types, search_term, should_fail)
             }
         } catch (error) {
             core.setFailed(error.message);
             return
         }
-
-        
     } catch (error) {
         core.setFailed(error.message);
     }
